9a4a08cbe7cfcfd89cd876d0b3c0f95b2274e961
Judy <paoc@umich.edu>
2020-08-09 20:48:22 -0700

    removed Next elements and used tailwind

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,37 +1,34 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/PreGame.module.css'
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+        <head>
+            <title>Mahjong</title>
+            <link rel="icon" href="/favicon.ico" />
+        </head>
+        <main className={styles.main}>
+            <h1 className={styles.title}>
+                Let's play Mahjong!
+            </h1>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Let's play Mahjong!
-        </h1>
+            <p className={styles.description}>
+                Don't know how to play? Check out the <a href="/rules" className={styles.description}> rules</a> here!
+            </p>
 
-        <p className={styles.description}>
-          Don't know how to play? Check out the <a href="/rules" className={styles.description}> rules</a> here!
-        </p>
+            <div className={styles.grid}>
+                <button href="/create_room" className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+                    New Room
+                </button>
+                <button href="/join_room" className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+                    Join a Room
+                </button>
+            </div>
+        </main>
 
-        <div className={styles.grid}>
-          <a href="/create_room" className={styles.card}>
-            <h3>New Room</h3>
-          </a>
-
-          <a href="/join_room" className={styles.card}>
-            <h3>Join a Room</h3>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-          Placeholder footer{' '}
-      </footer>
+        <footer className={styles.footer}>
+            Placeholder footer
+        </footer>
     </div>
   )
 }
---
+++ frontend/pages/lobby.js
@@ -0,0 +1,37 @@
+import Head from 'next/head'
+import styles from '../styles/preGame.css'
+
+export default function Lobby() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Mahjong</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Let's play Mahjong!
+        </h1>
+
+        <p className={styles.description}>
+          Don't know how to play? Check out the <a href="/rules" className={styles.description}> rules</a> here!
+        </p>
+
+        <div className={styles.grid}>
+          <a href="/create_room" className={styles.card}>
+            <h3>New Room</h3>
+          </a>
+
+          <a href="/join_room" className={styles.card}>
+            <h3>Join a Room</h3>
+          </a>
+        </div>
+      </main>
+
+      <footer className={styles.footer}>
+          Placeholder footer{' '}
+      </footer>
+    </div>
+  )
+}
